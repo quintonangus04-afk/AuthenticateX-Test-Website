54ff5d1637172edd951bfd78314ab23f409f0840
quintonangus04-afk <quintonangus04@gmail.com>
2026-02-10 23:06:30 +0000

    v

---
+++ verify.js
@@ -0,0 +1,31 @@
+const email = localStorage.getItem("pendingEmail");
+const codeInput = document.getElementById("codeInput");
+
+codeInput.addEventListener("keydown", async (e) => {
+    if (e.key === "Enter") {
+        code = codeInput.value;
+
+        verifyCode = await fetch("https://authenticatex-server-main-439442280694.europe-west1.run.app/verifyCode", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+            code: code,
+            email: email
+            })
+        })
+
+        const response = verifyCode.json()
+
+        if (response.success) {
+            console.log("Code verifed successfully")
+            window.location.href = "dashboard.html"
+        } else {
+            console.log("Unknown error ocoured")
+        }
+    }
+})
+
+    
+
+
+

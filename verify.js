4b7ee6da6c9a4e3c52e478409d909b6907f1a3db
quintonangus04-afk <quintonangus04@gmail.com>
2026-02-10 23:20:45 +0000

    Update verify.js

--- verify.js
+++ verify.js
@@ -3,9 +3,9 @@ const codeInput = document.getElementById("codeInput");
 
 codeInput.addEventListener("keydown", async (e) => {
     if (e.key === "Enter") {
-        code = codeInput.value;
+        const code = codeInput.value;
 
-        verifyCode = await fetch("https://authenticatex-server-main-439442280694.europe-west1.run.app/verifyCode", {
+        const verifyCode = await fetch("https://authenticatex-server-main-439442280694.europe-west1.run.app/verifyCode", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
